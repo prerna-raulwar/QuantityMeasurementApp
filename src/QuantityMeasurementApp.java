697e66bcbd51e737475db9eeaeec0f5b6fc8d02a
Prerna Raulwar <raulwarprerna@gmail.com>
2026-04-15 22:59:25 +0530

    UC3 DRY implemented

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,51 +1,39 @@
 public class QuantityMeasurementApp {
 
-    static class Feet {
+    static class Quantity {
         private final double value;
+        private final String unit;
 
-        public Feet(double value) {
+        public Quantity(double value, String unit) {
             this.value = value;
+            this.unit = unit;
+        }
+
+        private double toFeet() {
+            if (unit.equals("feet")) {
+                return value;
+            } else if (unit.equals("inch")) {
+                return value / 12;
+            }
+            return 0;
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
             if (obj == null || getClass() != obj.getClass()) return false;
-            Feet feet = (Feet) obj;
-            return Double.compare(this.value, feet.value) == 0;
+
+            Quantity other = (Quantity) obj;
+
+            return Double.compare(this.toFeet(), other.toFeet()) == 0;
         }
-    }
-
-    static class Inch {
-        private final double value;
-
-        public Inch(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Inch inch = (Inch) obj;
-            return Double.compare(this.value, inch.value) == 0;
-        }
-    }
-
-    public static boolean compareFeet(double v1, double v2) {
-        Feet f1 = new Feet(v1);
-        Feet f2 = new Feet(v2);
-        return f1.equals(f2);
-    }
-
-    public static boolean compareInch(double v1, double v2) {
-        Inch i1 = new Inch(v1);
-        Inch i2 = new Inch(v2);
-        return i1.equals(i2);
     }
 
     public static void main(String[] args) {
-        System.out.println("Feet Equal: " + compareFeet(1.0, 1.0));
-        System.out.println("Inch Equal: " + compareInch(1.0, 1.0));
+
+        Quantity q1 = new Quantity(1.0, "feet");
+        Quantity q2 = new Quantity(12.0, "inch");
+
+        System.out.println("Equal: " + q1.equals(q2));
     }
 }
