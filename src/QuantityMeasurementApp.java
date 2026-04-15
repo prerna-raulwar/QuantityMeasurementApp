7802925c0e8e475fb1f4e7213438c6dc6fd624c7
Prerna Raulwar <raulwarprerna@gmail.com>
2026-04-15 23:03:58 +0530

    UC4 extended units

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -10,17 +10,30 @@ public class QuantityMeasurementApp {
         }
 
         private double toFeet() {
+
             if (unit.equals("feet")) {
                 return value;
-            } else if (unit.equals("inch")) {
+            }
+
+            else if (unit.equals("inch")) {
                 return value / 12;
             }
+
+            else if (unit.equals("yard")) {
+                return value * 3;
+            }
+
+            else if (unit.equals("cm")) {
+                return value * 0.0328084;
+            }
+
             return 0;
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
+
             if (obj == null || getClass() != obj.getClass()) return false;
 
             Quantity other = (Quantity) obj;
@@ -31,9 +44,14 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        Quantity q1 = new Quantity(1.0, "feet");
-        Quantity q2 = new Quantity(12.0, "inch");
+        Quantity q1 = new Quantity(1.0, "yard");
+        Quantity q2 = new Quantity(3.0, "feet");
 
-        System.out.println("Equal: " + q1.equals(q2));
+        System.out.println("Yard vs Feet: " + q1.equals(q2));
+
+        Quantity q3 = new Quantity(1.0, "inch");
+        Quantity q4 = new Quantity(2.54, "cm");
+
+        System.out.println("Inch vs CM: " + q3.equals(q4));
     }
 }
