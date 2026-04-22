7e5e7551ac7cf09097b18e0f0d469686867466b9
Prerna Raulwar <raulwarprerna@gmail.com>
2026-04-22 09:48:53 +0530

    UC5 Length Conversion Implementation

---
+++ src/LengthUnit.java
@@ -0,0 +1,17 @@
+public enum LengthUnit {
+
+    FEET(1.0),
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(0.0328084);
+
+    private final double conversionFactor;
+
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+}
